f9f754e5b320a775bfb483800eb10e9162121c17
lexach91 <ronmctailor@protonmail.com>
2022-04-22 16:47:55 +0800

    Add jquery drag and drop functionality to bins and items

--- assets/js/game.js
+++ assets/js/game.js
@@ -103,3 +103,106 @@ let testJson = {
     apple: "./assets/test/apple.png",
   },
 };
+
+
+$(redBin).droppable({
+  drop: (event, ui) => {
+    let itemCategory = ui.draggable.attr("data-category");
+    if (itemCategory === "organic") {
+      $(ui.draggable).effect("explode", {
+        pieces: 50,
+        complete: () => {
+          $(ui.draggable).remove();
+        },
+      });
+      $(redBin).effect("bounce", {
+        times: 3,
+        distance: 20,
+      });
+
+      console.log("right");
+    } else {
+      $(ui.draggable).animate(
+        {
+          left: Math.floor(Math.random() * $(window).width()),
+          top: Math.floor(Math.random() * $(window).height()),
+        },
+        500
+      );
+      $(redBin).effect("shake", { times: 2 }, 500);
+      console.log("wrong");
+    }
+  },
+});
+
+$(blueBin).droppable({
+  drop: (event, ui) => {
+    let itemCategory = ui.draggable.attr("data-category");
+    if (itemCategory === "soft-plastic") {
+      $(ui.draggable).effect("explode", {
+        pieces: 50,
+        complete: () => {
+          $(ui.draggable).remove();
+        },
+      });
+      $(blueBin).effect("bounce", {
+        times: 3,
+        distance: 20,
+      });
+
+      console.log("right");
+    } else {
+      $(ui.draggable).animate(
+        {
+          left: Math.floor(Math.random() * $(window).width()),
+          top: Math.floor(Math.random() * $(window).height()),
+        },
+        500
+      );
+      $(blueBin).effect("shake", { times: 2 }, 500);
+      console.log("wrong");
+    }
+  },
+});
+
+$(greenBin).droppable({
+  drop: (event, ui) => {
+    let itemCategory = ui.draggable.attr("data-category");
+    if (itemCategory === "recyclable") {
+      $(ui.draggable).effect("explode", {
+        pieces: 50,
+        complete: () => {
+          $(ui.draggable).remove();
+        },
+      });
+      $(greenBin).effect("bounce", {
+        times: 3,
+        distance: 20,
+      });
+
+      console.log("right");
+    } else {
+      $(ui.draggable).animate(
+        {
+          left: Math.floor(Math.random() * $(window).width()),
+          top: Math.floor(Math.random() * $(window).height()),
+        },
+        500
+      );
+      $(greenBin).effect("shake", { times: 2 }, 500);
+      console.log("wrong");
+    }
+  },
+});
+
+const game = new Game(testJson);
+game.start("easy");
+
+$("img.garbage-item").draggable({
+  //   revert: "invalid",
+  //   helper: "",
+  cursor: "move",
+  start: (event, ui) => {
+    ui.helper.css("z-index", "100");
+  },
+});
